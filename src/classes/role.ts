9c1755ff67f07b51f1b37eb4baeb4a0151f3d333
Timothy Gillespie <Timothy.Gillespie@outlook.de>
2019-12-07 03:40:38 +0100

    Fill the missing ones in

--- src/classes/role.ts
+++ src/classes/role.ts
@@ -1,6 +1,33 @@
 enum Role{
+    
     HX = "Hexe",
     DB = "Dorfbewohner",
+    S = "Seher",
+    SL = "Seherlehrling",
+    A = "Amor",
+    BF = "Bärenführer",
+    AV = "Alte Vettel",
+    AM = "Alter Mann",
+    AS = "Aussätzige",
+    HB = "Harter Bursche",
+    J = "Jäger",
+    L = "Leibwächter",
+    M = "Magier",
+    P = "Priester",
+    R = "Ritter",
+    TB = "Trunkenbold",
+    U = "Unruhestifterin",
+
+    W = "Werwolf",
+    UW = "Urwolf",
+    WJ = "Wolfsjunges",
+
+    EW = "Einsamer Wolf",
+
+    V = "Verfluchter",
+    D = "Doppelgänger",
+
+    G = "Gerber",
 }
 
 export default Role;
