5f131887b72c7d0784c80f32239eecfec0eef68a
Timothy Gillespie <Timothy.Gillespie@outlook.de>
2019-12-10 19:35:16 +0100

    Change Leibwächter text und add more information

--- src/components/gamePhases/leibwachterProtects.tsx
+++ src/components/gamePhases/leibwachterProtects.tsx
@@ -15,7 +15,10 @@ class LeibwachterProtects extends React.Component<GamePhasePropsI, {}> {
 
     render() {
         return <div>
-            Wenn wird der Leibwächter {(getAlivePlayersOfRole(this.props.playersInTheGame, Role.L)[0]).getName()}?
+            Wenn wird der Leibwächter {(getAlivePlayersOfRole(this.props.playersInTheGame, Role.L)[0]).getName()} diese Nacht schützen?
+            <br />
+            {this.renderWhoCantBeProtected()}
+            <br />
             <br />
             {renderPlayerList(
                 difference(
@@ -30,6 +33,15 @@ class LeibwachterProtects extends React.Component<GamePhasePropsI, {}> {
                 }
             )}
         </div>
+    }
+
+
+    private renderWhoCantBeProtected(): JSX.Element {
+        // In this situation it refers to last night
+        if(this.props.protectedThisNight)
+            return <div>Er hat letzte nacht {this.props.protectedThisNight} geschützt und kann diese Person nicht nochmal schützen.</div>; 
+
+        return <React.Fragment></React.Fragment>;
     }
 
 }
